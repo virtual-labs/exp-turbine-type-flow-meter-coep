55e62950a5f8469fb05350549dcbcc5990730132
vleadadmin <admin@vlabs.ac.in>
2024-06-07 09:47:38 +0000

    https://virtual-labs.github.io/virtual-labs/exp-turbine-type-flow-meter-coep click on the link to test your code.

---
+++ simulation/src/Questions.js
@@ -0,0 +1,124 @@
+function Questions(){
+			var flag = false;
+			var myRadio = null;
+
+			$("#main-div-conf").html('');
+			
+			 $("#centerText1").html('BASIC KNOWLEDGE');
+			var questions = '';
+			questions += ''
+				+ '<div id = "questionDiv">'
+
+			for (var i = 0; i < QuestionsJSON.data["SEC"].length; i++) {
+
+				for (var j = 0; j < QuestionsJSON.data["SEC"][0].QUES.length; j++) {
+
+					questions += '<div class="col-md-12">'
+						+ '<br><span class="queno">Question No : ' + (i + 1) + '</span>'
+						+ '<br><div class="queTitle">'
+
+						+ QuestionsJSON.data["SEC"][i].QUES[j].QC
+						+ '</div>'
+
+					for (var k = 0; k < QuestionsJSON.data["SEC"][i].QUES[j].ANS.length; k++) {
+
+						questions += '<div>'
+							+ '<div class="ansTitle col-md-6">'
+
+							+ "<input class='radioType' name='radio-" + i + "' id='" + i + "' ANSID='" + QuestionsJSON.data["SEC"][i].QUES[j].ANS[k].ANSID + "' type='radio'"
+							+ "' value='"
+							+ QuestionsJSON.data["SEC"][i].QUES[j].ANS[k].content
+							+ "' >"
+							+ '<p style="margin: -20px 0 8px 29px;">'
+							+ QuestionsJSON.data["SEC"][i].QUES[j].ANS[k].content
+							+ '</p>'
+
+							+ '</div>'
+							+ '</div>'
+					}
+					questions += '</div>'
+				}
+			}
+
+			questions += '<div class="buttonDiv">'
+				+ '<button id="testSubmit" data-toggle="modal" data-target="#myModal">Submit Test</button>'
+				+ '</div>'
+				+ ' <div class="modal fade" id="myModal">'
+				+ '<div class="modal-dialog">'
+				+ '  <div class="modal-content">'
+			      
+				+ '    <!-- Modal Header -->'
+				+ '    <div class="modal-header bg-info">'
+				+ '       <h4 class="modal-title">Message box</h4>'
+				+ '       <button type="button" class="close" data-dismiss="modal">&times;</button>'
+				+ '     </div>'
+			        
+				+ '     <!-- Modal body -->'
+				+ '     <div class="modal-body" id="QuestionModel">'
+				
+				+ '     </div>'
+			        
+				+ '     <!-- Modal footer -->'
+				+ '     <div class="modal-footer">'
+				+ '       <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>'
+				+ '     </div>'
+			        
+				+ '  </div>'
+				+ '</div>'
+				+ ' </div>'
+				+ '</div>';
+
+			$("#main-div-conf").html(questions);
+
+			$('#testSubmit').on('click', function() {		
+
+					var arr = [];
+					
+					for (var i = 0; i < QuestionsJSON.data["SEC"].length; i++) {
+
+						var qid = $('input[name=radio-' + i + ']').attr(
+							'id');
+
+						var ansId = $('input[name=radio-' + i + ']:checked').attr(
+							'ANSID');
+
+						myRadio = $(
+							'input[name=radio-' + i + ']:checked')
+							.val();
+
+						if (myRadio == null) {
+							flag = flag && false;
+//							$("#QuestionModel").html("Please attempt all the questions");
+							str='<img src="images/cancel.png" class=" img-fluid " />'
+							    +'<b id="errorText" style="color:red;" >Please attempt all the questions </b> '
+						$("#QuestionModel").html(str);
+//							alert('Please attempt all the questions');
+
+							break;
+						}
+						arr.push({
+							"QID": qid,
+							"ANS": myRadio,
+							"ANSId": ansId
+						});
+					}
+
+					var ansCount = 0;
+
+					if (myRadio != null) {
+						for (var i = 0; i < arr.length; i++) {
+							if (arr[i].ANSId == "true") {
+								ansCount++;
+							}
+						}
+						
+						
+//						alert("Test Submitted Successfully <br/>Correct Answers Are : " + ansCount);
+						mainPage();
+						 str='<img src="images/checked.png" class=" img-fluid " />'
+							    +'<b id="errorText" style="color:green;" >Test Submitted Successfully .Correct Answers Are : ' + ansCount+'</b> '
+						$("#errorModel").html(str);
+					}
+			});
+}
+	
---
+++ simulation/src/rangeJson.js
@@ -0,0 +1,25 @@
+tempMasterJson = {};
+var masterJsonArr = [];
+var tempQuestionArr = [];
+var masterJson = {};
+
+masterJson = {
+	"srno": 1,
+	"input": "Amethyst",
+	"o": "mineral1",
+	"src": "images/Amethyst.jpg",
+	"chemical_Classification": "Silicate",
+	"colour": "Reddish Purple",
+	"streak": "Colourless (harder than the streak plate)",
+	"luster": "Vitreous",
+	"mohs_Hardness": "7",
+	"cleavage": "None - typically breaks with a conchoidal fracture",
+	"diaphaneity": "Transparent to translucent",
+	"gravity": "2.6 to 2.7",
+	"chemical_Composition": "SiO<sub>2</sub>",
+	"specialProperties": " Uses Faceted stones, cabochons, beads, tumbled stones, ornamental objects.",
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
